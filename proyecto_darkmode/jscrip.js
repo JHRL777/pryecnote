c2c69bcdec991ae4831d6977fb22c69f1bd0c564
JHRL777 <134742412+JHRL777@users.noreply.github.com>
2023-08-13 11:38:29 -0500

    practiva de css y js

--- proyecto_darkmode/jscrip.js
+++ proyecto_darkmode/jscrip.js
@@ -1,4 +1,7 @@
-let darkmode = document.getElementById('darkmode')
+let darkmode = document.getElementById('darkmode');
+let text= document.querySelector('.I-AM');
+let comentario = document.getElementById('comentario');
+
 
 darkmode.onclick = () =>{
     if(darkmode.classList.contains('bx-moon')){
@@ -11,3 +14,43 @@ darkmode.onclick = () =>{
 
     }
 }
+
+let objeto = [{
+    note: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        note: 'Aspernatur'
+    },
+    {
+        note: 'suscipit tenetur labore vitae ab impedit exercitationem in quibusdam veritatis aperiam expedita! Libero,'
+    },
+    {
+        note: 'voluptatibus'
+    },
+    {
+        note: 'Eum nam deserunt enim odit, nisi quis'
+    }
+
+]
+
+function obRndo(array){
+    let objetoRandom = Math.floor(Math.random()* array.length)
+    return array[objetoRandom]
+}
+
+
+console.log(objeto)
+
+comentario.addEventListener('click', crearobjeto, obRndo);
+
+function crearobjeto(){
+    let objetoAleatorio = obRndo(objeto);
+    console.log(objetoAleatorio);
+    let p = document.createElement('p');
+    p.classList.add('text')
+    p.innerHTML = `${objetoAleatorio.note}`
+    text.append(p)
+    
+}
+
+
